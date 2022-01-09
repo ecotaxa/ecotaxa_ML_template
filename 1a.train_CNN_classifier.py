539343815f043fa563f022c57287107352a96fd5
Jean-Olivier Irisson <irisson@normalesup.org>
2022-01-09 17:58:26 +0000

    Load the model from a given epoch before prediction

--- 1a.train_CNN_classifier.py
+++ 1a.train_CNN_classifier.py
@@ -163,6 +163,11 @@ history = cnn.Train(
 
 print('Evaluate model') ## ----
 
+# load model for best epoch
+best_epoch = 5  # use None to get latest epoch
+my_cnn,epoch = cnn.Load(ckpt_dir, epoch=best_epoch)
+print(' at epoch {:d}'.format(epoch))
+
 # predict classes for all dataset
 pred = cnn.Predict(
     model=my_cnn,
