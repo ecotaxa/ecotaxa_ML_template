d3d72ee2bb5839a52f1bf8b9699a2924b7f0c41d
Jean-Olivier Irisson <irisson@normalesup.org>
2022-01-11 21:52:31 +0000

    Improve training progress plots
    Add epochs as vertical lines (and other niceties)

--- monitor_training.R
+++ monitor_training.R
@@ -8,16 +8,19 @@ library("patchwork")
 
 # read training log
 input <- "io/checkpoints/training_log.tsv"
-d <- read_tsv(input, col_types = cols()) %>%
+d <- read_tsv(input, col_types = cols())
+dm <- d %>%
   # reformat to get losses and accuracies in the same column
   pivot_longer(cols=c(starts_with("train"), starts_with("val"))) %>%
   separate(name, into=c("dataset", "metric"), sep="_") %>%
   pivot_wider(names_from=metric, values_from=value)
+de <- filter(d, batch == min(batch))
 
 # define plots
-p_l <- ggplot(d) + geom_path(aes(x=step, y=loss, colour=dataset)) + theme(legend.position="top")
-p_a <- ggplot(d) + geom_path(aes(x=step, y=accuracy, colour=dataset)) + theme(legend.position="top")
-p_lr <- ggplot(filter(d, dataset=="train")) + geom_path(aes(x=step, y=learning_rate))
+base <- ggplot() + geom_vline(aes(xintercept=step), data=de, colour="grey80") + theme(legend.position="top")
+p_loss <- base + geom_path(aes(x=step, y=loss, colour=dataset), data=dm)
+p_accu <- base + geom_path(aes(x=step, y=accuracy, colour=dataset), data=dm)
+p_lr <- base + geom_path(aes(x=step, y=learning_rate), data=d)
 
 # plot in a nice grid
-(p_l + p_a) / p_lr + plot_layout(heights=c(3,1))
+(p_loss + p_accu) / p_lr + plot_layout(heights=c(2.5,1))
