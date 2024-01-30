c17ecffe7e4866b7deeff27b18aeb52e7d615c67
Jean-Olivier Irisson <irisson@normalesup.org>
2024-01-30 23:51:13 +0000

    Add simple python plots to follow along training

---
+++ monitor_training.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# Read the training log and plot the current stats
+#
+# (c) 2023 Jean-Olivier Irisson, GNU General Public License v3
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+df = pd.read_csv('io/checkpoints/training_log.tsv', sep='\t')
+df = df.drop(['batch', 'learning_rate'], axis='columns')
+
+df.plot(x='step', subplots=True)
+plt.show()
+
+df.plot(x='epoch', subplots=True)
+plt.show()
