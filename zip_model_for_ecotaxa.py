ae3dfe8dd5bca7173b96cee270b86594a28e3c11
Jean-Olivier Irisson <irisson@normalesup.org>
2024-04-15 21:59:05 +0000

    Add code to zip only the relevant files

---
+++ zip_model_for_ecotaxa.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from zipfile import ZipFile
+
+import pathlib
+fe1 = [f for f in pathlib.Path('io/feature_extractor/').iterdir() if f.is_file()]
+fe2 = [f for f in pathlib.Path('io/feature_extractor/variables/').iterdir() if f.is_file()]
+fe = fe1 + fe2
+
+# Create a ZipFile Object
+with ZipFile('io.zip', 'w') as zip_object:
+   # Adding files that need to be zipped
+   zip_object.write('io/crop.txt')
+   zip_object.write('io/dim_reducer.pickle')
+   for f in fe:
+       zip_object.write(f)
